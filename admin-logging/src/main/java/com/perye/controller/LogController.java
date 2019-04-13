2b736e4d9f89930f629c5b4c25bc38bec0469ce7
perye <11143526@qq.com>
2019-04-13 20:30:53 +0800

    日志

---
+++ admin-logging/src/main/java/com/perye/controller/LogController.java
@@ -0,0 +1,47 @@
+package com.perye.controller;
+
+import com.perye.domain.Log;
+import com.perye.service.query.LogQueryService;
+import com.perye.utils.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Perye
+ * @Date: 2019-04-13
+ */
+@RestController
+@RequestMapping("api")
+public class LogController {
+
+    @Autowired
+    private LogQueryService logQueryService;
+
+    @GetMapping(value = "/logs")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity getLogs(Log log, Pageable pageable){
+        log.setLogType("INFO");
+        return new ResponseEntity(logQueryService.queryAll(log,pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/logs/user")
+    public ResponseEntity getUserLogs(Log log, Pageable pageable){
+        log.setLogType("INFO");
+        log.setUsername(SecurityContextHolder.getUserDetails().getUsername());
+        return new ResponseEntity(logQueryService.queryAll(log,pageable), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/logs/error")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity getErrorLogs(Log log, Pageable pageable){
+        log.setLogType("ERROR");
+        return new ResponseEntity(logQueryService.queryAll(log,pageable), HttpStatus.OK);
+    }
+}
+
---
+++ admin-logging/src/main/java/com/perye/service/LogService.java
@@ -0,0 +1,20 @@
+package com.perye.service;
+
+import com.perye.domain.Log;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.scheduling.annotation.Async;
+
+/**
+ * @Author: Perye
+ * @Date: 2019-04-13
+ */
+public interface LogService {
+
+    /**
+     * 新增日志
+     * @param joinPoint
+     * @param log
+     */
+    @Async
+    void save(ProceedingJoinPoint joinPoint, Log log);
+}
